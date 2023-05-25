cf2ffc405df278854614e13c2bbacea213a22804
Gulnaz <gulnaz-0121@mail.ru>
2023-05-25 16:53:54 +0300

    Added new window and button

--- Template_4337/MainWindow.xaml.cs
+++ Template_4337/MainWindow.xaml.cs
@@ -30,5 +30,10 @@ namespace Template_4337
             var window = new Khuzyakaev_4337();
             window.Show();
         }
+        private void Yunusova_4337_Click(object sender, RoutedEventArgs e)
+        {
+            var gulnaz = new Yunusova_4337();
+            gulnaz.Show();
+        }
     }
 }
